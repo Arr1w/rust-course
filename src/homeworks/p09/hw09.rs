45e318f9786a996cc00eeab5ae5c882e6366e437
Bohdan Chepizhko <48531860+Arr1w@users.noreply.github.com>
2025-04-09 12:25:22 +0200

    Update hw09.rs

--- src/homeworks/p09/hw09.rs
+++ src/homeworks/p09/hw09.rs
@@ -11,3 +11,29 @@ fn rotate(s: String, n: isize) -> String {
     let (left, right) = s.split_at(s.len() - n);
     format!("{}{}", right, left)
 }
+
+#[test]
+fn test() {
+   let s = "abcdefgh";
+   let shifts = [
+       (0,  "abcdefgh"),
+       (8,  "abcdefgh"),
+       (-8, "abcdefgh"),
+       (1,  "habcdefg"),
+       (2,  "ghabcdef"),
+       (10, "ghabcdef"),
+       (-1, "bcdefgha"),
+       (-2, "cdefghab"),
+       (-10,"cdefghab"),
+   ];
+
+
+   shifts
+       .iter()
+       .for_each(|(n, exp)|
+           assert_eq!(
+               rotate2(s, n), 
+               exp.to_string()
+           )
+       );
+}
