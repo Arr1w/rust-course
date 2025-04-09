7a0a5228ab4d50f5a5d531de2357556cdbd265ac
Bohdan Chepizhko <48531860+Arr1w@users.noreply.github.com>
2025-04-09 12:25:42 +0200

    Update hw10.rs

--- src/homeworks/p10/hw10.rs
+++ src/homeworks/p10/hw10.rs
@@ -6,3 +6,20 @@ fn is_palindrome(x: u32) -> bool {
     let reversed_s: String = s.chars().rev().collect();
     s == reversed_s
 }
+
+#[test]
+fn test() {
+   let data =
+       [
+           (123, false),
+           (121, true),
+           (1221, true),
+       ];
+
+
+   data
+       .iter()
+       .for_each(|(n, exp)| {
+           assert_eq!(is_palindrome(*n), *exp);
+       });
+}
