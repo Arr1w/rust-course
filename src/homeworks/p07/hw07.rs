0646932a91652cb192e06324341a70031846bff3
Bohdan Chepizhko <48531860+Arr1w@users.noreply.github.com>
2025-04-09 12:24:22 +0200

    Update hw07.rs

--- src/homeworks/p07/hw07.rs
+++ src/homeworks/p07/hw07.rs
@@ -11,3 +11,26 @@ fn invert_the_case(s: String) -> String {
         })
         .collect()
 }
+
+#[test]
+fn test() {
+   let data =
+       [
+           ("Hello", "hELLO"),
+           ("Привет", "пРИВЕТ"),
+       ];
+
+
+   data
+       .iter()
+       .for_each(|(a, b)| {
+           assert_eq!(
+               invert_the_case(a.to_string()),
+               b.to_string()
+           );
+           assert_eq!(
+               invert_the_case(b.to_string()),
+               a.to_string()
+           );
+       });
+}
